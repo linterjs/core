3295c78e29666f441c2c62bdbeefba41b6b77743
Simon Fridlund <simon@fridlund.email>
2017-12-10 14:11:05 +0100

    Refactor lint function
    Use for...of to skip creating an intermediate array. Rename linterOutputList to lintOutput.
    Format file.

--- src/lint.ts
+++ src/lint.ts
@@ -1,5 +1,9 @@
 import { NoLintersError } from "./errors";
-import { linterMap, LinterAdapterLintOutput } from "./linter-map";
+import {
+  linterMap,
+  LinterAdapterLintOutput,
+  LinterAdapter
+} from "./linter-map";
 
 export interface LintInput {
   filePath?: string;
@@ -13,19 +17,17 @@ export function lint({ filePath, text }: LintInput): LintOutput {
     throw new NoLintersError();
   }
 
-  // Get a list of linters
-  const linters = Array.from(linterMap.values()).map(linterFactory =>
-    linterFactory()
-  );
+  const linters: LinterAdapter[] = [];
+  for (const linterFactory of linterMap.values()) {
+    linters.push(linterFactory());
+  }
 
   // Run text through all linters
   // XXX: Linting should be able to be done in parallel
-  const linterOutputList = linters.map(linter =>
-    linter.lint({ filePath, text })
-  );
+  const lintOutput = linters.map(linter => linter.lint({ filePath, text }));
 
   // XXX: Could we return a streaming result for the cli if we lint in parallel?
   // This way the cli could add the linter results for each file as they become
   // available.
-  return linterOutputList;
+  return lintOutput;
 }
