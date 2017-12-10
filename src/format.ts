23db5340f87748fe5236962a9bd24b2a8970fc1d
Simon Fridlund <simon@fridlund.email>
2017-12-10 14:15:38 +0100

    Initial implementation of format
    If there's a linter named prettier registered, run it before all other linters.

--- src/format.ts
+++ src/format.ts
@@ -1,13 +1,34 @@
-import { linterMap } from "./linter-map";
+import { linterMap, LinterAdapter } from "./linter-map";
+import { NoLintersError } from "./errors";
 
 export interface FormatInput {
   filePath?: string;
   text: string;
 }
 
-export interface FormatOutput {}
+export type FormatOutput = string;
 
-export function format({ filePath, text }: FormatInput): FormatOutput {
-  // TODO: Format all the things!
-  return {};
+export function format(input: FormatInput): FormatOutput {
+  if (linterMap.size === 0) {
+    throw new NoLintersError();
+  }
+
+  const linters: [string, LinterAdapter][] = [];
+  for (const [name, linterFactory] of linterMap) {
+    if (name === "prettier") {
+      linters.unshift([name, linterFactory()]);
+    } else {
+      linters.push([name, linterFactory()]);
+    }
+  }
+
+  const { text: formattedText } = linters.reduce(
+    ({ filePath, text }, [name, { format }]) => ({
+      ...(filePath && { filePath }),
+      text: format({ filePath, text })
+    }),
+    { ...input }
+  );
+
+  return formattedText;
 }
