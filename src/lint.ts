4b14509fe4bbf111a4a2a556bc35af46745189d2
Simon Fridlund <simon@fridlund.email>
2017-12-16 21:52:59 +0100

    Add an option to return an array of Promise<LinterAdapterLintOutput>

--- src/lint.ts
+++ src/lint.ts
@@ -7,28 +7,45 @@ import {
 
 export interface LintInput {
   filePath?: string;
+  returnArray: boolean;
   text: string;
 }
 
-export type LintOutput = LinterAdapterLintOutput[];
+export interface LintInputReturnArray extends LintInput {
+  returnArray: true;
+}
 
-export async function lint({ filePath, text }: LintInput): Promise<LintOutput> {
+export interface LintInputReturnPromise extends LintInput {
+  returnArray: false;
+}
+
+export type LintOutputArray = LinterAdapterLintOutput[];
+
+export type LintOutputPromise = Promise<LinterAdapterLintOutput[]>;
+
+export type LintOutput = LintOutputArray | LintOutputPromise;
+
+export function lint(args: LintInputReturnArray): LintOutputArray;
+export function lint(args: LintInputReturnPromise): LintOutputPromise;
+export function lint({ filePath, text, returnArray }: LintInput): LintOutput {
   if (linterMap.size === 0) {
     throw new NoLintersError();
   }
 
-  const linters: LinterAdapter[] = [];
+  const linters: (LinterAdapter | Promise<LinterAdapter>)[] = [];
   for (const linterFactory of linterMap.values()) {
-    linters.push(await linterFactory());
+    linters.push(linterFactory());
   }
 
   // Run text through all linters
   // XXX: Linting should be able to be done in parallel
   const lintArgs = { filePath, text };
-  const lintOutput = linters.map(async linter => await linter.lint(lintArgs));
+  const lintOutput = linters.map(
+    async linter => await (await linter).lint(lintArgs)
+  );
 
   // XXX: Could we return a streaming result for the cli if we lint in parallel?
   // This way the cli could add the linter results for each file as they become
   // available.
-  return Promise.all(lintOutput);
+  return returnArray ? lintOutput : Promise.all(lintOutput);
 }
