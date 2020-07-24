9964bf5d133b802c030f2f4520ed6700fd57ee50
Christopher Quadflieg <chrissi92@hotmail.de>
2020-07-24 22:33:05 +0200

    Add types for loglevel-colored-level-prefix

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,8 @@
 import getLogger = require("loglevel-colored-level-prefix");
+import { LogLevelDesc } from "loglevel";
 
-export function getDefaultLogLevel() {
-  return process.env.LOG_LEVEL || "warn";
+export function getDefaultLogLevel(): LogLevelDesc {
+  return (process.env.LOG_LEVEL as LogLevelDesc | undefined) ?? "warn";
 }
 
 export const logger = getLogger({ prefix: "@linter/core" });
--- src/loglevel-colored-level-prefix.d.ts
+++ src/loglevel-colored-level-prefix.d.ts
@@ -1,1 +1,12 @@
-declare module "loglevel-colored-level-prefix";
+declare module "loglevel-colored-level-prefix" {
+  import { Logger, LogLevelDesc } from "loglevel";
+
+  namespace log {
+    export interface LoggerOptions {
+      level?: LogLevelDesc;
+      prefix?: string;
+    }
+  }
+  function getLogger(options?: log.LoggerOptions): Logger;
+  export = getLogger;
+}
