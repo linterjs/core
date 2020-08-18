0669986eefc4f1fb209182840497c8871dd041a1
Christopher Quadflieg <chrissi92@hotmail.de>
2020-08-18 20:53:20 +0200

    Prefix log messages with package name

--- src/logger.ts
+++ src/logger.ts
@@ -2,10 +2,10 @@ import { createLogger, format, transports } from "winston";
 
 export const logger = createLogger({
   level: "warn",
-  // defaultMeta: {
-  //   prefix: "@linter/core",
-  // },
   transports: new transports.Console({
-    format: format.simple(),
+    format: format.combine(
+      format.label({ label: "@linter/core", message: true }),
+      format.simple(),
+    ),
   }),
 });
